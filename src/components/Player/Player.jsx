5c8c03795b026af5360b85c784446bc3a3db7a4f
Ali Majidi <amajidi1382@gmail.com>
2024-05-02 14:43:32 +0330

    feat: Complete change username feature

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,15 +1,44 @@
+import { useState } from "react";
+
 import editIcon from "./../../assets/icons/edit.png";
+import checkIcon from "./../../assets/icons/check.png";
 import "./style.css";
 
-function Player({ children: name, symbol, isActive }) {
+function Player({ children: initialName, symbol, isActive }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function changePlayerName(e) {
+        setPlayerName(prevName => e.target.value.trim() || prevName);
+    }
+
+    function toggleIsEditing() {
+        setIsEditing(wasEditing => !wasEditing);
+    }
+
+    function submitHandler(e) {
+        e.preventDefault();
+        toggleIsEditing();
+    }
+
     return (
-        <div className={`player ${symbol} ${isActive ? "active" : ""}`}>
+        <form className={`player ${symbol} ${isActive ? "active" : ""}`} onSubmit={submitHandler}>
             <span className="player__symbol">{symbol}</span>
-            <span className="player__name">
-                {name}
-                <img className="player__edit" src={editIcon} />
-            </span>
-        </div>
+            {!isEditing && <span className="player__name">{playerName}</span>}
+            {isEditing && (
+                <input
+                    type="text"
+                    className="changeNameInput"
+                    defaultValue={playerName}
+                    onChange={changePlayerName}
+                />
+            )}
+            <img
+                className={`player__edit ${isEditing ? "editing" : ""}`}
+                src={isEditing ? checkIcon : editIcon}
+                onClick={toggleIsEditing}
+            />
+        </form>
     );
 }
 
