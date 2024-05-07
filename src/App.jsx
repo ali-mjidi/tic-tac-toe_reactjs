1ed985b36424b7693f4753f2290e5b8c3a990d19
Ali Majidi <amajidi1382@gmail.com>
2024-05-07 19:26:22 +0330

    feat: Complete winning or draw logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import GameBoard from "./components/GameBoard/GameBoard";
 import Player from "./components/Player/Player";
 
+import { WINNING_CONDITIONS } from "./winning_conditions";
 import "./App.css";
 
 const initialBoard = [
@@ -11,6 +12,12 @@ const initialBoard = [
     [null, null, null],
     [null, null, null],
 ];
+
+function deriveActivePlayer(logs) {
+    const activePlayer = logs.length > 0 && logs[0].player === "X" ? "O" : "X";
+
+    return activePlayer;
+}
 
 function updateBoard(gameLogs, gameBoard) {
     for (const log of gameLogs) {
@@ -25,14 +32,34 @@ function updateBoard(gameLogs, gameBoard) {
     return gameBoard;
 }
 
+function checkWinner(board) {
+    for (const condition of WINNING_CONDITIONS) {
+        const firstSquare = board[condition[0].row][condition[0].column];
+        const secondSquare = board[condition[1].row][condition[1].column];
+        const thirdSquare = board[condition[2].row][condition[2].column];
+
+        if (firstSquare && firstSquare === secondSquare && firstSquare === thirdSquare) {
+            return firstSquare;
+        }
+    }
+}
+
 function App() {
     const [logs, setLogs] = useState([]);
+    const activePlayer = deriveActivePlayer(logs);
     let gameBoard = [...initialBoard.map(innerArray => [...innerArray])];
+    let winner = undefined;
+    let isDraw = logs.length === 9 && !winner;
 
     gameBoard = updateBoard(logs, gameBoard);
+    winner = checkWinner(gameBoard);
 
     function resetHandler() {
         setLogs([]);
+    }
+
+    if (winner || isDraw) {
+        alert(winner);
     }
 
     return (
@@ -42,10 +69,10 @@ function App() {
             <GameBoard gameLogs={logs} changeLogs={setLogs} gameBoard={gameBoard} />
 
             <footer className="playersInfo">
-                <Player symbol="X" isActive={logs[0] ? logs[0].player === "X" : true}>
+                <Player symbol="X" isActive={activePlayer === "X"}>
                     Player 1
                 </Player>
-                <Player symbol="O" isActive={logs[0]?.player === "O"}>
+                <Player symbol="O" isActive={activePlayer === "O"}>
                     Player 2
                 </Player>
             </footer>
